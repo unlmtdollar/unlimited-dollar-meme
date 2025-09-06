5f3a37071bc01667054877d8ef37f1192b0b25d9
unlmtdollar <unlmtdollar@gmail.com>
2025-09-06 18:19:56 +0300

    Update snap.js

--- snap.js
+++ snap.js
@@ -72,7 +72,7 @@ async function connectWallet() {
     token   = new ethers.Contract(TOKEN_CONTRACT_ADDRESS, TOKEN_ABI, provider);
 
     const network = await provider.getNetwork();
-    networkNameElem.textContent = network.chainId === 97453 ? "Sidra Chain" : "Unknown Network";
+    networkNameElem.textContent = network.chainId === 369 ? "Pulse Chain" : "Unknown Network";
 
     await updateWalletInfo();
   } catch (err) {
